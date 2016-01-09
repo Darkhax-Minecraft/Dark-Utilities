2853f520f580140f8debbed1e85ea850a957800b
Darkhax <darklime@live.ca>
2016-01-09 02:27:40 -0700

    Added a basic ContentHandler to put some stuff into.

---
+++ src/main/java/net/darkhax/darkutils/client/handler/ContentHandler.java
@@ -0,0 +1,16 @@
+package net.darkhax.darkutils.client.handler;
+
+public class ContentHandler {
+    
+    public static void initBlocks () {
+    
+    }
+    
+    public static void initItems () {
+    
+    }
+    
+    public static void initRecipes () {
+    
+    }
+}
