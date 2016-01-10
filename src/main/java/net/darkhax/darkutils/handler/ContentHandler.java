e7003fa7628797f55752676992cb4db6d35eaeb3
Darkhax <darklime@live.ca>
2016-01-10 07:56:43 -0700

    Moved to correct place.

--- src/main/java/net/darkhax/darkutils/handler/ContentHandler.java
+++ src/main/java/net/darkhax/darkutils/handler/ContentHandler.java
@@ -1,18 +1,25 @@
-package net.darkhax.darkutils.client.handler;
+package net.darkhax.darkutils.handler;
 
+import net.darkhax.darkutils.blocks.BlockEnderTether;
 import net.darkhax.darkutils.blocks.BlockTrapTile;
 import net.darkhax.darkutils.items.ItemBlockBasic;
+import net.darkhax.darkutils.tileentity.TileEntityEnderTether;
 import net.minecraft.block.Block;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class ContentHandler {
     
     public static Block blockTrap;
+    public static Block blockEnderTether;
     
     public static void initBlocks () {
         
         blockTrap = new BlockTrapTile();
         GameRegistry.registerBlock(blockTrap, ItemBlockBasic.class, "trap_tile", new Object[] { BlockTrapTile.EnumType.getTypes() });
+        
+        blockEnderTether = new BlockEnderTether();
+        GameRegistry.registerBlock(blockEnderTether, "ender_tether");
+        GameRegistry.registerTileEntity(TileEntityEnderTether.class, "ender_tether");
         
     }
     
