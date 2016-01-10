f1284893ca92ae7621fdcdf8e07f79063d302623
Darkhax <darklime@live.ca>
2016-01-10 05:19:44 -0700

    Added JEI support

---
+++ src/main/java/net/darkhax/darkutils/addons/jei/DarkUtilsJEIPlugin.java
@@ -0,0 +1,47 @@
+package net.darkhax.darkutils.addons.jei;
+
+import mezz.jei.api.IItemRegistry;
+import mezz.jei.api.IJeiHelpers;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.IModRegistry;
+import mezz.jei.api.IRecipeRegistry;
+import mezz.jei.api.JEIPlugin;
+import net.darkhax.darkutils.blocks.BlockTrapTile;
+import net.darkhax.darkutils.client.handler.ContentHandler;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
+
+@JEIPlugin
+public class DarkUtilsJEIPlugin implements IModPlugin {
+    
+    public static IJeiHelpers jeiHelper;
+    
+    @Override
+    public void register (IModRegistry registry) {
+
+        for (BlockTrapTile.EnumType type : BlockTrapTile.EnumType.values())
+            registry.addDescription(new ItemStack(ContentHandler.blockTrap, 1, type.meta ), "jei.darkutils.trap." + type.type + ".desc");
+    }
+    
+    @Override
+    public void onJeiHelpersAvailable (IJeiHelpers jeiHelpers) {
+        
+        jeiHelper = jeiHelpers;
+    }
+    
+    @Override
+    public void onItemRegistryAvailable (IItemRegistry itemRegistry) {
+    
+    }
+    
+    @Override
+    public void onRecipeRegistryAvailable (IRecipeRegistry recipeRegistry) {
+    
+    }
+
+    @Override
+    public boolean isModLoaded () {
+        
+        return true;
+    }
+}
