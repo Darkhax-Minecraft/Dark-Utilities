58e51959d1a5c82e4e7e664f05b3021b6f414008
Darkhax <darklime@live.ca>
2016-01-20 16:34:51 -0700

    Imports.

--- src/main/java/net/darkhax/darkutils/libs/Utilities.java
+++ src/main/java/net/darkhax/darkutils/libs/Utilities.java
@@ -1,6 +1,5 @@
 package net.darkhax.darkutils.libs;
 
-import net.minecraft.block.Block;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
