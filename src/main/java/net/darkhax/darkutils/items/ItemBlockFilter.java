221714eb3cb07d11550f794e4e2f79c1575f5172
Darkhax <darklime@live.ca>
2016-01-20 22:25:49 -0700

    Created an ItemBlock for the filter.

---
+++ src/main/java/net/darkhax/darkutils/items/ItemBlockFilter.java
@@ -0,0 +1,33 @@
+package net.darkhax.darkutils.items;
+
+import java.util.List;
+
+import net.minecraft.block.Block;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.util.StatCollector;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class ItemBlockFilter extends ItemBlockBasic {
+    
+    public ItemBlockFilter(Block block, String[] names) {
+        
+        super(block, names);
+    }
+    
+    @Override
+    public String getUnlocalizedName (ItemStack stack) {
+        
+        return super.getUnlocalizedName();
+    }
+    
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void addInformation (ItemStack stack, EntityPlayer playerIn, List<String> tooltip, boolean advanced) {
+        
+        if (!(stack.getMetadata() > names.length))
+            tooltip.add(StatCollector.translateToLocal("tooltip.filter.type") + ": " + EnumChatFormatting.AQUA + StatCollector.translateToLocal("tooltip.filter.type." + names[stack.getMetadata()]));
+    }
+}
