646a01cf8029d4b5a29d672d04502d04056c4e02
Darkhax <darklime@live.ca>
2016-01-24 22:41:58 -0700

    Set up postInit support for addons.

--- src/main/java/net/darkhax/darkutils/DarkUtils.java
+++ src/main/java/net/darkhax/darkutils/DarkUtils.java
@@ -1,6 +1,7 @@
 package net.darkhax.darkutils;
 
 import net.darkhax.darkutils.addons.AddonHandler;
+import net.darkhax.darkutils.addons.thaumcraft.DarkUtilsThaumcraftAddon;
 import net.darkhax.darkutils.common.ProxyCommon;
 import net.darkhax.darkutils.creativetab.CreativeTabDarkUtils;
 import net.darkhax.darkutils.handler.ContentHandler;
@@ -11,9 +12,10 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = Constants.MOD_ID, name = Constants.MOD_NAME, version = Constants.VERSION_NUMBER, acceptedMinecraftVersions = Constants.MCVERSION)
+@Mod(modid = Constants.MOD_ID, name = Constants.MOD_NAME, version = Constants.VERSION_NUMBER, dependencies = Constants.DEPENDENCIES, acceptedMinecraftVersions = Constants.MCVERSION)
 public class DarkUtils {
     
     @SidedProxy(clientSide = Constants.CLIENT_PROXY_CLASS, serverSide = Constants.SERVER_PROXY_CLASS)
@@ -34,5 +36,11 @@ public class DarkUtils {
         proxy.onPreInit();
         MinecraftForge.EVENT_BUS.register(new ForgeEventHandler());
         AddonHandler.preInit();
+    }
+    
+    @EventHandler
+    public void postInit (FMLPostInitializationEvent event) {
+        
+        AddonHandler.postInit();
     }
 }
