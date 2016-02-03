b14a6fc90eccb3f5760cb8c20ff71fc8ce74db27
Darkhax <darklime@live.ca>
2016-02-03 14:09:37 -0700

    Added a basic addon for Chisel.

---
+++ src/main/java/net/darkhax/darkutils/addons/chisel/DarkUtilsChiselAddon.java
@@ -0,0 +1,28 @@
+package net.darkhax.darkutils.addons.chisel;
+
+import net.minecraft.block.Block;
+import team.chisel.api.carving.CarvingUtils;
+
+public class DarkUtilsChiselAddon {
+    
+    public static Block blockShelf;
+    
+    public static void onPreInit () {
+    
+    }
+    
+    /**
+     * Registers a Block with the Chisel mod. This method is intended for use with blocks that
+     * have multiple variants, such as wool or planks.
+     * 
+     * @param group: The group to add this block to.
+     * @param block: The block to add to the group.
+     * @param maxMeta: The highest meta used for the block you want to add. If your block has 5
+     *            variants using meta 0 through 4, you will want to use 5.
+     */
+    private static void addCarvingVariant (String group, Block block, int maxMeta) {
+        
+        for (int meta = 0; meta < maxMeta; meta++)
+            CarvingUtils.chisel.addVariation(group, block.getStateFromMeta(meta), 5);
+    }
+}
