62b74bdc27a2a68cd572fa522f928aec5d563ebc
Darkhax <darklime@live.ca>
2016-02-27 17:28:28 -0700

    Fixed rendering not updating immediately.

--- src/main/java/net/darkhax/darkutils/tileentity/TileEntitySneaky.java
+++ src/main/java/net/darkhax/darkutils/tileentity/TileEntitySneaky.java
@@ -4,11 +4,18 @@ import net.darkhax.bookshelf.tileentity.TileEntityBasic;
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.play.server.S35PacketUpdateTileEntity;
 
 public class TileEntitySneaky extends TileEntityBasic {
     
+    /**
+     * A BlockState held by the TileEntity. The block will try it's best to camo into the held
+     * state.
+     */
     public IBlockState heldState;
     
+    @Override
     public void readNBT (NBTTagCompound tag) {
         
         Block heldBlock = Block.getBlockFromName(tag.getString("HeldBlockId"));
@@ -17,6 +24,7 @@ public class TileEntitySneaky extends TileEntityBasic {
             heldState = heldBlock.getStateFromMeta(tag.getInteger("HeldBlockMeta"));
     }
     
+    @Override
     public void writeNBT (NBTTagCompound tag) {
         
         if (heldState != null) {
@@ -24,5 +32,13 @@ public class TileEntitySneaky extends TileEntityBasic {
             tag.setString("HeldBlockId", Block.blockRegistry.getNameForObject(heldState.getBlock()).toString());
             tag.setInteger("HeldBlockMeta", heldState.getBlock().getMetaFromState(heldState));
         }
+    }
+    
+    @Override
+    public void onDataPacket (NetworkManager net, S35PacketUpdateTileEntity packet) {
+        
+        super.onDataPacket(net, packet);
+        readNBT(packet.getNbtCompound());
+        worldObj.markBlockRangeForRenderUpdate(pos, pos);
     }
 }
