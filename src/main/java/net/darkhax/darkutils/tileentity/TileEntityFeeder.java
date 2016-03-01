facf4289fe28146d138e178476d4926e2e9a0d57
Darkhax <darklime@live.ca>
2016-02-29 17:38:34 -0700

    Added the love updating logic.

--- src/main/java/net/darkhax/darkutils/tileentity/TileEntityFeeder.java
+++ src/main/java/net/darkhax/darkutils/tileentity/TileEntityFeeder.java
@@ -1,27 +1,33 @@
 package net.darkhax.darkutils.tileentity;
 
+import java.util.List;
+
+import net.darkhax.bookshelf.lib.util.EntityUtils;
 import net.darkhax.bookshelf.lib.util.ItemStackUtils;
 import net.darkhax.bookshelf.tileentity.TileEntityBasic;
 import net.darkhax.darkutils.handler.ContentHandler;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.passive.EntityAnimal;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.MobSpawnerBaseLogic;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.IChatComponent;
 import net.minecraft.util.ITickable;
 
-public class TileEntityFeeder extends TileEntityBasic implements IInventory{
-
+public class TileEntityFeeder extends TileEntityBasic implements IInventory {
+    
     private int food = 0;
     
-    public int addFood(ItemStack foodStack) {
+    public int addFood (ItemStack foodStack) {
         
         if (this.food == getSizeInventory())
             return foodStack.stackSize;
-        
+            
         setFood(this.food += foodStack.stackSize);
         
         if (this.food > getSizeInventory()) {
@@ -34,19 +40,37 @@ public class TileEntityFeeder extends TileEntityBasic implements IInventory{
         return 0;
     }
     
-    public int getFood() {
+    public int getFood () {
         
         return this.food;
     }
     
-    public void setFood(int food) {
+    public void setFood (int food) {
         
         this.food = food;
         this.worldObj.setBlockState(this.pos, getStateFromFood());
         this.markDirty();
     }
     
-    public IBlockState getStateFromFood() {
+    @Override
+    public void onEntityUpdate () {
+        
+        if (!this.worldObj.isRemote && this.food != 0) {
+            
+            List<EntityAnimal> animals = EntityUtils.getEntitiesInArea(EntityAnimal.class, this.getWorld(), this.getPos(), 8);
+            
+            for (EntityAnimal animal : animals) {
+                
+                if (animal.getGrowingAge() == 0 && animal.inLove <= 0) {
+                    
+                    animal.inLove = 1200;
+                    this.getWorld().setEntityState(animal, (byte) 18);
+                }
+            }
+        }
+    }
+    
+    public IBlockState getStateFromFood () {
         
         return ContentHandler.blockFeeder.getStateFromMeta(food);
     }
@@ -59,7 +83,7 @@ public class TileEntityFeeder extends TileEntityBasic implements IInventory{
     
     @Override
     public void readNBT (NBTTagCompound dataTag) {
-    
+        
         this.food = dataTag.getInteger("FoodAmount");
     }
     
@@ -68,71 +92,71 @@ public class TileEntityFeeder extends TileEntityBasic implements IInventory{
         
         return "tile.darkutils.feeder.name";
     }
-
+    
     @Override
     public boolean hasCustomName () {
         
         return false;
     }
-
+    
     @Override
     public IChatComponent getDisplayName () {
         
         return null;
     }
-
+    
     @Override
     public int getSizeInventory () {
         
         return 10;
     }
-
+    
     @Override
     public ItemStack getStackInSlot (int index) {
         
         return null;
     }
-
+    
     @Override
     public ItemStack decrStackSize (int index, int count) {
         
         return null;
     }
-
+    
     @Override
     public ItemStack removeStackFromSlot (int index) {
         
         return null;
     }
-
+    
     @Override
     public void setInventorySlotContents (int index, ItemStack stack) {
-          
+        
         stack.stackSize = addFood(stack);
     }
-
+    
     @Override
     public int getInventoryStackLimit () {
         
         return 1;
     }
-
+    
     @Override
     public boolean isUseableByPlayer (EntityPlayer player) {
         
         return false;
     }
-
+    
     @Override
     public void openInventory (EntityPlayer player) {
-        
+    
     }
-
+    
     @Override
     public void closeInventory (EntityPlayer player) {
-        
+    
     }
-
+    
     @Override
     public boolean isItemValidForSlot (int index, ItemStack stack) {
         
@@ -144,24 +168,24 @@ public class TileEntityFeeder extends TileEntityBasic implements IInventory{
         
         return false;
     }
-
+    
     @Override
     public int getField (int id) {
         
         return 0;
     }
-
+    
     @Override
     public void setField (int id, int value) {
-        
+    
     }
-
+    
     @Override
     public int getFieldCount () {
         
         return 0;
     }
-
+    
     @Override
     public void clear () {
         
