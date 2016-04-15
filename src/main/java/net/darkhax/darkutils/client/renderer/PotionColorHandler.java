802c801d5201bf4ae7d97df9e5444e365d61e22f
Tyler Hancock <darklime@live.ca>
2016-04-14 23:20:39 -0600

    Actually implement color handler.

---
+++ src/main/java/net/darkhax/darkutils/client/renderer/PotionColorHandler.java
@@ -0,0 +1,13 @@
+package net.darkhax.darkutils.client.renderer;
+
+import net.minecraft.client.renderer.color.IItemColor;
+import net.minecraft.item.ItemStack;
+
+public class PotionColorHandler implements IItemColor {
+    
+    @Override
+    public int getColorFromItemstack (ItemStack stack, int renderPass) {
+        
+        return renderPass > 0 ? 16777215 : (stack.getMetadata() == 0) ? 16710911 : 7371335;
+    }
+}
