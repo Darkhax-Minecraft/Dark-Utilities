1874040466372cf9bda2256acac3e848f97aded6
Tyler Hancock <darklime@live.ca>
2016-04-14 23:20:52 -0600

    Fixed timer not having model.

--- src/main/java/net/darkhax/darkutils/blocks/BlockTimer.java
+++ src/main/java/net/darkhax/darkutils/blocks/BlockTimer.java
@@ -14,6 +14,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.AxisAlignedBB;
@@ -122,5 +123,11 @@ public class BlockTimer extends BlockContainer {
     public BlockRenderLayer getBlockLayer () {
         
         return BlockRenderLayer.CUTOUT;
+    }
+    
+    @Override
+    public EnumBlockRenderType getRenderType (IBlockState state) {
+        
+        return EnumBlockRenderType.MODEL;
     }
 }
