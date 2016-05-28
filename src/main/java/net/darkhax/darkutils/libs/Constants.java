13fb2bc0ab0453f722d26870af7626872770e933
Tyler Hancock <darklime@live.ca>
2016-05-28 00:20:54 -0600

    Removed Thaumcraft support.

--- src/main/java/net/darkhax/darkutils/libs/Constants.java
+++ src/main/java/net/darkhax/darkutils/libs/Constants.java
@@ -9,10 +9,10 @@ public class Constants {
     
     public static final String MOD_ID = "DarkUtils";
     public static final String MOD_NAME = "Dark Utilities";
-    public static final String VERSION_NUMBER = "1.1.0.0";
+    public static final String VERSION_NUMBER = "1.1.1.0";
     public static final String CLIENT_PROXY_CLASS = "net.darkhax.darkutils.client.ProxyClient";
     public static final String SERVER_PROXY_CLASS = "net.darkhax.darkutils.common.ProxyCommon";
-    public static final String DEPENDENCIES = "required-after:bookshelf@[1.2.1.251,];after:Waila;after:Thaumcraft;after:JEI@[3.4.0.204,];";
+    public static final String DEPENDENCIES = "required-after:bookshelf@[1.2.1.251,];after:Waila;after:JEI@[3.4.0.204,];";
     public static final Random RANDOM = new Random();
     public static final Logger LOGGER = LogManager.getLogger(MOD_NAME);
 }
