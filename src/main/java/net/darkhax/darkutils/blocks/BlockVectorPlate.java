54e422b72c3ef9388c35da45e9d985c9eb427cb4
Tyler Hancock <darklime@live.ca>
2016-06-02 03:25:03 -0600

    Cleaned up vector plate code significantly. Fixed item despawning.

--- src/main/java/net/darkhax/darkutils/blocks/BlockVectorPlate.java
+++ src/main/java/net/darkhax/darkutils/blocks/BlockVectorPlate.java
@@ -1,7 +1,5 @@
 package net.darkhax.darkutils.blocks;
 
-import net.darkhax.bookshelf.lib.util.EntityUtils;
-import net.darkhax.bookshelf.lib.util.MathsUtils;
 import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.properties.PropertyDirection;
 import net.minecraft.block.state.BlockStateContainer;
@@ -11,7 +9,6 @@ import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 public class BlockVectorPlate extends BlockTrapBase {
@@ -25,35 +22,34 @@ public class BlockVectorPlate extends BlockTrapBase {
         super();
         this.setUnlocalizedName("darkutils.trap.movement");
         this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
-        this.slipperiness = 0.98F;
         this.speed = speed;
     }
     
     @Override
     public void onEntityCollidedWithBlock (World world, BlockPos pos, IBlockState state, Entity entity) {
         
-        if (!(entity instanceof EntityLivingBase || entity instanceof EntityItem) || entity.isSneaking())
-            return;
-            
-        final EnumFacing direction = state.getValue(FACING);
+        final EnumFacing direction = state.getValue(FACING).getOpposite();
         
-        if (this.requireCentering(pos, entity.getPositionVector(), direction))
-            this.centerDirectional(pos, entity, direction);
+        if (entity.canBePushed() && !entity.isSneaking()) {
             
-        if (entity != null)
-            EntityUtils.pushTowards(entity, state.getValue(FACING), this.speed);
-    }
-    
-    @Override
-    public IBlockState onBlockPlaced (World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
-        
-        return this.getDefaultState().withProperty(FACING, placer.getHorizontalFacing().getOpposite());
+            entity.motionX += this.speed * direction.getFrontOffsetX();
+            entity.motionZ += this.speed * direction.getFrontOffsetZ();
+            
+            if (entity instanceof EntityItem)
+                ((EntityItem) entity).setAgeToCreativeDespawnTime();
+        }
     }
     
     @Override
     protected BlockStateContainer createBlockState () {
         
         return new BlockStateContainer(this, new IProperty[] { FACING });
+    }
+    
+    @Override
+    public IBlockState onBlockPlaced (World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
+        
+        return this.getDefaultState().withProperty(FACING, placer.getHorizontalFacing().getOpposite());
     }
     
     @Override
@@ -66,24 +62,5 @@ public class BlockVectorPlate extends BlockTrapBase {
     public int getMetaFromState (IBlockState state) {
         
         return state.getValue(FACING).getHorizontalIndex();
-    }
-    
-    private void centerDirectional (BlockPos block, Entity entity, EnumFacing facing) {
-        
-        final Vec3d entPos = entity.getPositionVector();
-        
-        if (facing == EnumFacing.EAST || facing == EnumFacing.WEST)
-            entity.setPositionAndUpdate(entPos.xCoord, entPos.yCoord, block.getZ() + 0.5);
-            
-        else
-            entity.setPositionAndUpdate(block.getX() + 0.5, entPos.yCoord, entPos.zCoord);
-    }
-    
-    private boolean requireCentering (BlockPos block, Vec3d entPos, EnumFacing facing) {
-        
-        if (facing == EnumFacing.EAST || facing == EnumFacing.WEST)
-            return !MathsUtils.isInRange(block.getZ() + 0.35, block.getZ() + 0.65, entPos.zCoord);
-            
-        return !MathsUtils.isInRange(block.getX() + 0.35, block.getX() + 0.65, entPos.xCoord);
     }
 }
