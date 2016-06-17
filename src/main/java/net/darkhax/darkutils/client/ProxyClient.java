c406016ebd9a3a4637fc9e2d249256ffc753b7f8
Tyler Hancock <darklime@live.ca>
2016-06-17 11:57:27 -0600

    Changed how models are registered. Much cleaner now.

--- src/main/java/net/darkhax/darkutils/client/ProxyClient.java
+++ src/main/java/net/darkhax/darkutils/client/ProxyClient.java
@@ -10,7 +10,6 @@ import net.darkhax.darkutils.common.ProxyCommon;
 import net.darkhax.darkutils.entity.EntityFakeTNT;
 import net.darkhax.darkutils.handler.ContentHandler;
 import net.darkhax.darkutils.items.ItemMaterial;
-import net.darkhax.darkutils.items.ItemMysteriousPotion;
 import net.darkhax.darkutils.tileentity.TileEntityShulkerPearl;
 import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
@@ -24,6 +23,9 @@ import net.minecraftforge.fml.client.registry.RenderingRegistry;
 
 public class ProxyClient extends ProxyCommon {
     
+    /**
+     * A state map instance for the sneaky block model.
+     */
     private static final StateMapperBase SNEAKY_MAP = new StateMapSneaky();
     
     @Override
@@ -31,60 +33,27 @@ public class ProxyClient extends ProxyCommon {
         
         MinecraftForge.EVENT_BUS.register(new ClientEventHandler());
         
-        Item item;
+        // Block Models
+        registerBlockInvModel(ContentHandler.blockTrap, BlockTrapEffect.EnumType.getTypes());
+        registerBlockInvModel(ContentHandler.blockEnderTether, 0);
+        registerBlockInvModel(ContentHandler.blockTrapMovement, 0);
+        registerBlockInvModel(ContentHandler.blockTrapMovementFast, 0);
+        registerBlockInvModel(ContentHandler.blockTrapMovementHyper, 0);
+        registerBlockInvModel(ContentHandler.blockGrate, 0);
+        registerBlockInvModel(ContentHandler.blockFilter, BlockFilter.EnumType.getTypes());
+        registerBlockInvModel(ContentHandler.blockTimer, 0);
+        registerBlockInvModel(ContentHandler.blockAntiSlime, 0);
+        registerBlockInvModel(ContentHandler.blockDetector, 0);
+        registerBlockInvModel(ContentHandler.blockCakeEPlus, 0);
+        registerBlockInvModel(ContentHandler.blockFakeTNT, 0);
         
-        item = Item.getItemFromBlock(ContentHandler.blockTrap);
+        // Item Models
+        registerItemInvModel(ContentHandler.itemMaterial, ItemMaterial.varients);
+        registerItemInvModel(ContentHandler.itemPotion, 0, "bottle_drinkable");
+        registerItemInvModel(ContentHandler.itemPotion, 1, "bottle_drinkable");
+        registerItemInvModel(ContentHandler.itemCharmPortal, 0);
         
-        for (final BlockTrapEffect.EnumType varient : BlockTrapEffect.EnumType.values())
-            ModelLoader.setCustomModelResourceLocation(item, varient.meta, new ModelResourceLocation("darkutils:trap_" + varient.type, "inventory"));
-            
-        item = Item.getItemFromBlock(ContentHandler.blockEnderTether);
-        ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation("darkutils:ender_tether", "inventory"));
-        
-        item = Item.getItemFromBlock(ContentHandler.blockTrapMovement);
-        ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation("darkutils:trap_move", "inventory"));
-        
-        item = Item.getItemFromBlock(ContentHandler.blockTrapMovementFast);
-        ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation("darkutils:trap_move_fast", "inventory"));
-        
-        item = Item.getItemFromBlock(ContentHandler.blockTrapMovementHyper);
-        ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation("darkutils:trap_move_hyper", "inventory"));
-        
-        item = Item.getItemFromBlock(ContentHandler.blockGrate);
-        ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation("darkutils:grate", "inventory"));
-        
-        item = Item.getItemFromBlock(ContentHandler.blockFilter);
-        for (final BlockFilter.EnumType varient : BlockFilter.EnumType.values())
-            ModelLoader.setCustomModelResourceLocation(item, varient.meta, new ModelResourceLocation("darkutils:filter_" + varient.type, "inventory"));
-            
-        item = Item.getItemFromBlock(ContentHandler.blockTimer);
-        ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation("darkutils:timer", "inventory"));
-        
-        item = Item.getItemFromBlock(ContentHandler.blockAntiSlime);
-        ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation("darkutils:anti_slime", "inventory"));
-        
-        item = Item.getItemFromBlock(ContentHandler.blockDetector);
-        ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation("darkutils:detector", "inventory"));
-        
-        item = Item.getItemFromBlock(ContentHandler.blockCakeEPlus);
-        ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation("darkutils:cake_eplus", "inventory"));
-        
-        item = Item.getItemFromBlock(ContentHandler.blockFakeTNT);
-        ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation("darkutils:fake_tnt", "inventory"));
-        
-        for (int materialMeta = 0; materialMeta < ItemMaterial.varients.length; materialMeta++)
-            ModelLoader.setCustomModelResourceLocation(ContentHandler.itemMaterial, materialMeta, new ModelResourceLocation("darkutils:material_" + ItemMaterial.varients[materialMeta], "inventory"));
-            
-        for (int meta = 0; meta < ItemMysteriousPotion.varients.length; meta++)
-            ModelLoader.setCustomModelResourceLocation(ContentHandler.itemPotion, meta, new ModelResourceLocation("bottle_drinkable", "inventory"));
-            
-        item = Item.getItemFromBlock(ContentHandler.blockFeeder);
-        ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation("darkutils:feeder_empty", "inventory"));
-        ModelLoader.setCustomModelResourceLocation(item, 10, new ModelResourceLocation("darkutils:feeder_full", "inventory"));
-        
-        for (int meta = 1; meta < 10; meta++)
-            ModelLoader.setCustomModelResourceLocation(item, meta, new ModelResourceLocation("darkutils:feeder_partial", "inventory"));
-            
+        // Sneaky Block Models
         this.registerSneakyModel(ContentHandler.blockSneakyBlock, "sneaky_default", false);
         this.registerSneakyModel(ContentHandler.blockSneakyLever, "sneaky_lever", false);
         this.registerSneakyModel(ContentHandler.blockSneakyGhost, "sneaky_default", true);
@@ -110,6 +79,70 @@ public class ProxyClient extends ProxyCommon {
     
     }
     
+    /**
+     * Registers inventory models for a block that uses meta data.
+     * 
+     * @param block The block to register models for.
+     * @param variants The names of the models to use in order of meta data.
+     */
+    private void registerBlockInvModel (Block block, String[] variants) {
+        
+        registerItemInvModel(Item.getItemFromBlock(block), variants);
+    }
+    
+    /**
+     * Registers inventory models for a block.
+     * 
+     * @param block The block to register models for.
+     * @param meta The meta data to register the model for.
+     */
+    private void registerBlockInvModel (Block block, int meta) {
+        
+        registerItemInvModel(Item.getItemFromBlock(block), meta);
+    }
+    
+    /**
+     * Registers inventory models for an item.
+     * 
+     * @param item The item to register a model for.
+     * @param meta The meta data to register the model for.
+     * @param model The name of the model to register.
+     */
+    private void registerItemInvModel (Item item, int meta, String model) {
+        
+        ModelLoader.setCustomModelResourceLocation(item, meta, new ModelResourceLocation(model, "inventory"));
+    }
+    
+    /**
+     * Registers inventory models for an item that uses meta data.
+     * 
+     * @param item The item to register a model for.
+     * @param variants The names of the models to use, in order of meta data.
+     */
+    private void registerItemInvModel (Item item, String[] variants) {
+        
+        for (int meta = 0; meta < variants.length; meta++)
+            ModelLoader.setCustomModelResourceLocation(item, meta, new ModelResourceLocation(item.getRegistryName().getResourceDomain() + ":" + variants[meta], "inventory"));
+    }
+    
+    /**
+     * Registers inventory models for an item.
+     * 
+     * @param item The item to registers a model for.
+     * @param meta The meta data to register the model for.
+     */
+    private void registerItemInvModel (Item item, int meta) {
+        
+        ModelLoader.setCustomModelResourceLocation(item, meta, new ModelResourceLocation(item.getRegistryName().toString(), "inventory"));
+    }
+    
+    /**
+     * Registers a sneaky block model.
+     * 
+     * @param block The block to register the model for.
+     * @param name The name of the sneaky block model.
+     * @param useDefault Whether or not the default model should be used.
+     */
     private void registerSneakyModel (Block block, String name, boolean useDefault) {
         
         final Item item = Item.getItemFromBlock(block);
