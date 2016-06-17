4aa5f6f23db5ce89a97033bd9d0029d5f7f85576
Tyler Hancock <darklime@live.ca>
2016-06-17 13:06:14 -0600

    Base item for the portal charm.

---
+++ src/main/java/net/darkhax/darkutils/items/ItemPortalCharm.java
@@ -0,0 +1,12 @@
+package net.darkhax.darkutils.items;
+
+import net.darkhax.darkutils.DarkUtils;
+import net.minecraft.item.Item;
+
+public class ItemPortalCharm extends Item {
+    
+    public ItemPortalCharm() {
+        
+        this.setCreativeTab(DarkUtils.TAB);
+    }
+}
