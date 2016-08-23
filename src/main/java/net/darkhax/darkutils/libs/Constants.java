a60123320f900e6f255cd5ed58201674d2e1655a
Tyler Hancock <darklime@live.ca>
2016-08-22 23:18:05 -0600

    Updated dependency requirements

--- src/main/java/net/darkhax/darkutils/libs/Constants.java
+++ src/main/java/net/darkhax/darkutils/libs/Constants.java
@@ -12,7 +12,7 @@ public class Constants {
     public static final String VERSION_NUMBER = "1.1.3.0";
     public static final String CLIENT_PROXY_CLASS = "net.darkhax.darkutils.client.ProxyClient";
     public static final String SERVER_PROXY_CLASS = "net.darkhax.darkutils.common.ProxyCommon";
-    public static final String DEPENDENCIES = "required-after:bookshelf@[1.2.3.275,);after:Waila;after:JEI;";
+    public static final String DEPENDENCIES = "required-after:bookshelf@[1.3.0.288,);after:Waila;after:JEI;";
     public static final Random RANDOM = new Random();
     public static final Logger LOGGER = LogManager.getLogger(MOD_NAME);
 }
