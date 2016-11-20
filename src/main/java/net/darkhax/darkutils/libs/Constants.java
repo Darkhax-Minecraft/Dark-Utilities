10c7975e9fe6b69d07ed1e4c66fce75fac593695
Tyler Hancock <darklime@live.ca>
2016-11-20 05:38:46 -0700

    Upticked build and dependency versions

--- src/main/java/net/darkhax/darkutils/libs/Constants.java
+++ src/main/java/net/darkhax/darkutils/libs/Constants.java
@@ -9,10 +9,10 @@ public class Constants {
     
     public static final String MOD_ID = "darkutils";
     public static final String MOD_NAME = "Dark Utilities";
-    public static final String VERSION_NUMBER = "1.1.5.0";
+    public static final String VERSION_NUMBER = "1.1.6.0";
     public static final String CLIENT_PROXY_CLASS = "net.darkhax.darkutils.client.ProxyClient";
     public static final String SERVER_PROXY_CLASS = "net.darkhax.darkutils.common.ProxyCommon";
-    public static final String DEPENDENCIES = "required-after:bookshelf@[1.4.2.330,);after:Waila;after:JEI;";
+    public static final String DEPENDENCIES = "required-after:bookshelf@[1.4.3.338,);after:Waila;after:JEI;";
     public static final Random RANDOM = new Random();
     public static final Logger LOGGER = LogManager.getLogger(MOD_NAME);
 }
