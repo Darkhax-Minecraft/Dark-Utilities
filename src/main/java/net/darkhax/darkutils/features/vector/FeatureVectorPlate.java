42a9e90702b19c3b6928c76faff02febf25fe8c1
Tyler Hancock <darklime@live.ca>
2017-02-14 12:27:44 -0700

    Vector plates upgrades are no longer free!

--- src/main/java/net/darkhax/darkutils/features/vector/FeatureVectorPlate.java
+++ src/main/java/net/darkhax/darkutils/features/vector/FeatureVectorPlate.java
@@ -4,14 +4,17 @@ import static net.darkhax.bookshelf.lib.util.OreDictUtils.SLIMEBALL;
 import static net.darkhax.bookshelf.lib.util.OreDictUtils.STONE;
 
 import net.darkhax.darkutils.features.Feature;
+import net.darkhax.darkutils.features.material.FeatureMaterial;
 import net.darkhax.darkutils.libs.ModUtils;
 import net.minecraft.block.Block;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.ShapedRecipes;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.oredict.OreDictionary;
 import net.minecraftforge.oredict.ShapedOreRecipe;
 
 public class FeatureVectorPlate extends Feature {
@@ -24,12 +27,6 @@ public class FeatureVectorPlate extends Feature {
 
     private static boolean craftVectorPlate = true;
 
-    private static boolean convertNormalFast = true;
-
-    private static boolean convertFastHyper = true;
-
-    private static boolean convertHyperNormal = true;
-
     protected static boolean preventItemDespawn = true;
 
     protected static boolean preventItemPickup = true;
@@ -39,8 +36,6 @@ public class FeatureVectorPlate extends Feature {
     private static double fastSpeed = 0.3d;
 
     private static double hyperSpeed = 1.5d;
-
-    private static int craftAmount = 8;
 
     @Override
     public void onPreInit () {
@@ -59,34 +54,21 @@ public class FeatureVectorPlate extends Feature {
     public void setupConfiguration (Configuration config) {
 
         craftVectorPlate = config.getBoolean("Craft Vector Plate", this.configName, true, "Should the Vector Plate be craftable?");
-        convertNormalFast = config.getBoolean("Convert Normal to Fast", this.configName, true, "Should normal vector plates be convertable to fast vector plates?");
-        convertFastHyper = config.getBoolean("Convert Fast to Hyper", this.configName, true, "Should fast vector plates be convertable to hyper vector plates?");
-        convertHyperNormal = config.getBoolean("Convert Hyper to Fast", this.configName, true, "Should hyper vector plates be convertable to normal vector plates?");
         preventItemDespawn = config.getBoolean("Prevent Item Despawn", this.configName, true, "Should vector plates prevent item despawn?");
         preventItemPickup = config.getBoolean("Prevent Item Pickup", this.configName, true, "Should vector plates prevent items from being picked up, while they are being pushed?");
         normalSpeed = config.getFloat("Normal Speed", this.configName, 0.06f, 0f, 5f, "Speed modifier for the normal vector plate");
         fastSpeed = config.getFloat("Fast Speed", this.configName, 0.3f, 0f, 5f, "Speed modifier for the fast vector plate");
         hyperSpeed = config.getFloat("Hyper Speed", this.configName, 1.5f, 0f, 5f, "Speed modifier for the hyper vector plate");
-        craftAmount = config.getInt("Crafting Amount", this.configName, 8, 1, 64, "The amount of vector plates to give per recipe set");
     }
 
     @Override
     public void setupRecipes () {
 
         if (craftVectorPlate) {
-            GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(blockVectorPlate, craftAmount), new Object[] { "isi", "bfb", 's', SLIMEBALL, 'b', STONE, 'f', Items.SUGAR }));
-        }
-
-        if (convertNormalFast) {
-            ModUtils.addConversionRecipes(new ItemStack(blockVectorPlate), new ItemStack(blockFastVectorPlate, 1, 0));
-        }
-
-        if (convertFastHyper) {
-            ModUtils.addConversionRecipes(new ItemStack(blockFastVectorPlate), new ItemStack(blockHyperVectorPlate, 1, 0));
-        }
-
-        if (convertHyperNormal) {
-            ModUtils.addConversionRecipes(new ItemStack(blockHyperVectorPlate), new ItemStack(blockVectorPlate, 1, 0));
+            
+            GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(blockVectorPlate, 8), new Object[] { "isi", "bfb", 's', SLIMEBALL, 'b', STONE, 'f', Items.SUGAR }));
+            GameRegistry.addShapedRecipe(new ItemStack(blockFastVectorPlate, 8), "xxx", "xyx", "xxx", 'x', blockVectorPlate, 'y', ModUtils.validateCrafting(new ItemStack(FeatureMaterial.itemMaterial, 1, 3)));
+            GameRegistry.addShapedRecipe(new ItemStack(blockHyperVectorPlate, 8), "xxx", "xyx", "xxx", 'x', blockFastVectorPlate, 'y', ModUtils.validateCrafting(new ItemStack(FeatureMaterial.blockWitherDust, 1, OreDictionary.WILDCARD_VALUE)));
         }
     }
 
