feac8d9be6a9a04237eed735757f904f4d564750
Tyler Hancock <darklime@live.ca>
2017-03-09 11:28:00 -0700

    Fixed vector plates resetting their state. Closes #50
    Build with this fix should be on Curse automagically within a few
    minutes.

--- src/main/java/net/darkhax/darkutils/features/vector/BlockVectorPlate.java
+++ src/main/java/net/darkhax/darkutils/features/vector/BlockVectorPlate.java
@@ -126,6 +126,12 @@ public class BlockVectorPlate extends Block {
     }
 
     @Override
+    public IBlockState getStateFromMeta (int meta) {
+
+        return this.getDefaultState().withProperty(FACING, EnumFacing.getHorizontal(meta));
+    }
+
+    @Override
     public int getMetaFromState (IBlockState state) {
 
         return state.getValue(FACING).getHorizontalIndex();
