58aac9a10990b1e090578ccff41e660ba4bf3915
Tyler Hancock <darklime@live.ca>
2017-11-25 23:09:15 -0700

    Cleaned up anti slime loop.

--- src/main/java/net/darkhax/darkutils/features/antislime/FeatureAntiSlime.java
+++ src/main/java/net/darkhax/darkutils/features/antislime/FeatureAntiSlime.java
@@ -1,8 +1,5 @@
 package net.darkhax.darkutils.features.antislime;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import net.darkhax.darkutils.DarkUtils;
 import net.darkhax.darkutils.features.DUFeature;
 import net.darkhax.darkutils.features.Feature;
@@ -37,9 +34,8 @@ public class FeatureAntiSlime extends Feature {
 
         if (event.getEntity() instanceof EntitySlime && !event.getEntity().hasCustomName()) {
 
-            final List<TileEntity> tiles = new ArrayList<>(event.getWorld().loadedTileEntityList);
+            for (final TileEntity tile : event.getWorld().loadedTileEntityList) {
 
-            for (final TileEntity tile : tiles) {
                 if (tile instanceof TileEntityAntiSlime && ((TileEntityAntiSlime) tile).shareChunks((EntityLivingBase) event.getEntity())) {
 
                     if (event.getWorld().isBlockPowered(tile.getPos())) {
