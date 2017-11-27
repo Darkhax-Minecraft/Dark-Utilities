9f8dfbe4d173360276f5c1048f654e5892e8ff34
Tyler Hancock <darklime@live.ca>
2017-11-26 23:22:44 -0700

    Updated how JEI information works.

--- src/main/java/net/darkhax/darkutils/addons/jei/DarkUtilsJEIPlugin.java
+++ src/main/java/net/darkhax/darkutils/addons/jei/DarkUtilsJEIPlugin.java
@@ -1,26 +1,53 @@
 package net.darkhax.darkutils.addons.jei;
 
-import mezz.jei.api.BlankModPlugin;
+import mezz.jei.api.IModPlugin;
 import mezz.jei.api.IModRegistry;
 import mezz.jei.api.JEIPlugin;
+import net.darkhax.bookshelf.registry.IVariant;
+import net.darkhax.bookshelf.util.GameUtils;
 import net.darkhax.darkutils.DarkUtils;
-import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.translation.I18n;
 import net.minecraftforge.oredict.OreDictionary;
 
 @JEIPlugin
-public class DarkUtilsJEIPlugin extends BlankModPlugin {
+public class DarkUtilsJEIPlugin implements IModPlugin {
 
     @Override
     public void register (IModRegistry registry) {
 
-        for (final Block block : DarkUtils.REGISTRY.getBlocks()) {
-            registry.addDescription(new ItemStack(block, 1, OreDictionary.WILDCARD_VALUE), "jei." + block.getUnlocalizedName());
-        }
-
         for (final Item item : DarkUtils.REGISTRY.getItems()) {
-            registry.addDescription(new ItemStack(item, 1, OreDictionary.WILDCARD_VALUE), "jei." + item.getUnlocalizedName());
+
+            if (item instanceof IVariant) {
+
+                final String[] variants = ((IVariant) item).getVariant();
+
+                for (int meta = 0; meta < variants.length; meta++) {
+
+                    final String key = "jei." + item.getUnlocalizedName() + "." + variants[meta];
+                    registry.addIngredientInfo(new ItemStack(item, 1, meta), ItemStack.class, key);
+                    this.validateKey(key);
+                }
+            }
+
+            else {
+
+                final String key = "jei." + item.getUnlocalizedName();
+                registry.addIngredientInfo(new ItemStack(item, 1, OreDictionary.WILDCARD_VALUE), ItemStack.class, key);
+                this.validateKey(key);
+            }
+        }
+    }
+
+    private void validateKey (String key) {
+
+        if (GameUtils.isClient()) {
+
+            if (!I18n.canTranslate(key)) {
+
+                DarkUtils.LOGGER.info("Could not translate: " + key);
+            }
         }
     }
 }
