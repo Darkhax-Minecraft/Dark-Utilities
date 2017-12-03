4ca8a5dded58fa6bbe66683ac84d1cfec1191131
Tyler Hancock <darklime@live.ca>
2017-12-03 02:56:14 -0700

    Wither dust can be used to turn skeletons into wither skeletons.

--- src/main/java/net/darkhax/darkutils/features/material/ItemMaterial.java
+++ src/main/java/net/darkhax/darkutils/features/material/ItemMaterial.java
@@ -1,6 +1,15 @@
 package net.darkhax.darkutils.features.material;
 
 import net.darkhax.bookshelf.item.ItemSubType;
+import net.darkhax.bookshelf.util.StackUtils;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.monster.AbstractSkeleton;
+import net.minecraft.entity.monster.EntityWitherSkeleton;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumHand;
+import net.minecraft.world.World;
 
 public class ItemMaterial extends ItemSubType {
 
@@ -9,5 +18,34 @@ public class ItemMaterial extends ItemSubType {
     public ItemMaterial () {
 
         super(varients);
+    }
+
+    @Override
+    public boolean itemInteractionForEntity (ItemStack stack, EntityPlayer playerIn, EntityLivingBase target, EnumHand hand) {
+
+        final World world = target.getEntityWorld();
+
+        if (!world.isRemote && stack.getMetadata() == 0 && target instanceof AbstractSkeleton && !(target instanceof EntityWitherSkeleton)) {
+
+            final EntityWitherSkeleton witherSkeleton = new EntityWitherSkeleton(world);
+            witherSkeleton.copyLocationAndAnglesFrom(target);
+            witherSkeleton.setHealth(target.getHealth());
+
+            for (final EntityEquipmentSlot slot : EntityEquipmentSlot.values()) {
+
+                if (target.hasItemInSlot(slot)) {
+
+                    witherSkeleton.setItemStackToSlot(slot, target.getItemStackFromSlot(slot));
+                    target.setItemStackToSlot(slot, ItemStack.EMPTY);
+                    StackUtils.decreaseStackSize(stack, 1);
+                }
+            }
+
+            world.removeEntity(target);
+            world.spawnEntity(witherSkeleton);
+            return true;
+        }
+
+        return false;
     }
 }
