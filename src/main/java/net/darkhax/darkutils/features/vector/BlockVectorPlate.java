e20290d990a4c0fbbdde52998922f7133d9d4ab2
Tyler Hancock <darklime@live.ca>
2017-12-03 02:00:48 -0700

    Added the ability to shift click rotate a vector plate. #113

--- src/main/java/net/darkhax/darkutils/features/vector/BlockVectorPlate.java
+++ src/main/java/net/darkhax/darkutils/features/vector/BlockVectorPlate.java
@@ -9,7 +9,9 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
@@ -45,6 +47,18 @@ public class BlockVectorPlate extends Block {
         else {
             return true;
         }
+    }
+
+    @Override
+    public boolean onBlockActivated (World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+
+        if (playerIn.isSneaking()) {
+
+            worldIn.setBlockState(pos, state.cycleProperty(FACING));
+            return true;
+        }
+
+        return false;
     }
 
     @Override
