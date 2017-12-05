7ae899b08c77025c2969529df80a1c0f8f59c7b9
Tyler Hancock <darklime@live.ca>
2017-12-04 17:16:20 -0700

    Removed the null charm tooltip.

--- src/main/java/net/darkhax/darkutils/features/charms/ItemNullCharm.java
+++ src/main/java/net/darkhax/darkutils/features/charms/ItemNullCharm.java
@@ -1,19 +1,14 @@
 package net.darkhax.darkutils.features.charms;
 
-import java.util.List;
-
 import net.darkhax.bookshelf.item.ItemInventory;
 import net.darkhax.bookshelf.util.StackUtils;
 import net.darkhax.darkutils.DarkUtils;
 import net.darkhax.darkutils.handler.GuiHandler;
-import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumHand;
 import net.minecraft.world.World;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class ItemNullCharm extends ItemCharm {
 
@@ -25,17 +20,6 @@ public class ItemNullCharm extends ItemCharm {
         }
 
         return super.onItemRightClick(world, player, handIn);
-    }
-
-    @Override
-    @SideOnly(Side.CLIENT)
-    public void addInformation (ItemStack stack, World world, List<String> tooltip, ITooltipFlag advanced) {
-
-        for (final ItemStack blacklisted : ItemInventory.getContents(stack)) {
-            if (!blacklisted.isEmpty()) {
-                tooltip.add(blacklisted.getDisplayName());
-            }
-        }
     }
 
     public static boolean isBlackListed (ItemStack stack, ItemStack charmStack) {
