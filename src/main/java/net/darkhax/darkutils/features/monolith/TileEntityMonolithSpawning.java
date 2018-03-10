60274ff7b2a4f5a47bb23e55047b8827310d40b5
Tyler Hancock <darklime@live.ca>
2018-03-09 23:32:09 -0700

    Overhauled the monolith of spawning behaviour.

--- src/main/java/net/darkhax/darkutils/features/monolith/TileEntityMonolithSpawning.java
+++ src/main/java/net/darkhax/darkutils/features/monolith/TileEntityMonolithSpawning.java
@@ -6,8 +6,14 @@ import net.darkhax.bookshelf.data.AttributeOperation;
 import net.darkhax.bookshelf.util.WorldUtils;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EnumCreatureType;
+import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.ai.attributes.ModifiableAttributeInstance;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.world.World;
 import net.minecraft.world.WorldServer;
 
 public class TileEntityMonolithSpawning extends TileEntityMonolith {
@@ -18,24 +24,54 @@ public class TileEntityMonolithSpawning extends TileEntityMonolith {
     private static final AttributeModifier SPAWN_BONUS = new AttributeModifier(UUID.fromString("4d59c07e-abea-480c-a237-c9bafb5161e2"), "monolith_spawn_bonus", 0.2, AttributeOperation.MULTIPLY.ordinal());
 
     private int spawnCount = 0;
+    private BlockPos down = null;
 
     @Override
     public void onEntityUpdate () {
 
         super.onEntityUpdate();
 
+        if (this.down == null) {
+
+            this.down = this.getPos().down();
+        }
+
         if (this.getWorld() instanceof WorldServer) {
 
-            for (int attempts = 0; attempts < 25; attempts++) {
+            for (int attempts = 0; attempts < 5; attempts++) {
 
-                WorldUtils.attemptChunkSpawn((WorldServer) this.getWorld(), this.pos, EnumCreatureType.MONSTER, TileEntityMonolithSpawning::buffMob);
+                WorldUtils.attemptChunkSpawn((WorldServer) this.getWorld(), this.pos, EnumCreatureType.MONSTER, this::buffMob);
             }
         }
     }
 
-    private static void buffMob (EntityLiving living) {
+    private void buffMob (EntityLiving living) {
 
-        // System.out.println("Additional Spawn: " + living.getName());
+        final ModifiableAttributeInstance attack = (ModifiableAttributeInstance) living.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE);
+        final ModifiableAttributeInstance health = (ModifiableAttributeInstance) living.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH);
+
+        if (attack != null && health != null) {
+
+            if (!attack.hasModifier(SPAWN_BONUS)) {
+
+                attack.applyModifier(SPAWN_BONUS);
+            }
+
+            if (!health.hasModifier(SPAWN_BONUS)) {
+
+                health.applyModifier(SPAWN_BONUS);
+                living.setHealth(living.getMaxHealth());
+            }
+        }
+
+        this.spawnCount++;
+    }
+
+    @Override
+    public boolean onBlockActivated (World worldIn, EntityPlayer playerIn) {
+
+        playerIn.sendStatusMessage(new TextComponentTranslation("chat.darkutils.monolith.spawning", this.spawnCount), true);
+        return true;
     }
 
     @Override
