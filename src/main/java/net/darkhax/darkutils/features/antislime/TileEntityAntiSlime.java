b8f2b7fc0e4d71af18ca6feb4b18ed65b8de7c0c
Tyler Hancock <darklime@live.ca>
2018-03-18 11:54:44 -0600

    Fix a potential CME in the anti slime block.

--- src/main/java/net/darkhax/darkutils/features/antislime/TileEntityAntiSlime.java
+++ src/main/java/net/darkhax/darkutils/features/antislime/TileEntityAntiSlime.java
@@ -1,20 +1,13 @@
 package net.darkhax.darkutils.features.antislime;
 
+import net.darkhax.bookshelf.util.WorldUtils;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.MathHelper;
-import net.minecraft.util.math.Vec3d;
-import net.minecraft.world.chunk.Chunk;
 
 public class TileEntityAntiSlime extends TileEntity {
 
     public boolean shareChunks (EntityLivingBase entity) {
 
-        final Vec3d entPos = entity.getPositionVector();
-        final BlockPos blockpos = new BlockPos(MathHelper.floor(entPos.x), 0, MathHelper.floor(entPos.z));
-        final Chunk chunk = entity.world.getChunkFromBlockCoords(blockpos);
-        final Chunk tileChunk = this.world.getChunkFromBlockCoords(this.pos);
-        return chunk.x == tileChunk.x && chunk.z == tileChunk.z;
+        return WorldUtils.areSameChunk(entity.getPosition(), this.getPos());
     }
 }
