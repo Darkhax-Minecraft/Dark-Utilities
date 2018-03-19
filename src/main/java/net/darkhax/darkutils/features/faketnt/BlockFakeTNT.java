4339b9cda8f4c986701d4a707ec3b29feebec717
Tyler Hancock <darklime@live.ca>
2018-03-18 21:26:30 -0600

    Fixed real tnt turning fake tnt into real tnt.

--- src/main/java/net/darkhax/darkutils/features/faketnt/BlockFakeTNT.java
+++ src/main/java/net/darkhax/darkutils/features/faketnt/BlockFakeTNT.java
@@ -8,6 +8,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.Explosion;
 import net.minecraft.world.World;
 
 public class BlockFakeTNT extends BlockTNT {
@@ -22,13 +23,22 @@ public class BlockFakeTNT extends BlockTNT {
     @Override
     public void explode (World worldIn, BlockPos pos, IBlockState state, EntityLivingBase igniter) {
 
-        if (!worldIn.isRemote) {
-            if (state.getValue(EXPLODE).booleanValue()) {
+        if (!worldIn.isRemote && state.getValue(EXPLODE).booleanValue()) {
 
-                final EntityFakeTNT entitytntprimed = new EntityFakeTNT(worldIn, pos.getX() + 0.5F, pos.getY(), pos.getZ() + 0.5F, igniter);
-                worldIn.spawnEntity(entitytntprimed);
-                worldIn.playSound((EntityPlayer) null, entitytntprimed.posX, entitytntprimed.posY, entitytntprimed.posZ, SoundEvents.ENTITY_TNT_PRIMED, SoundCategory.BLOCKS, 1.0F, 1.0F);
-            }
+            final EntityFakeTNT entitytntprimed = new EntityFakeTNT(worldIn, pos.getX() + 0.5F, pos.getY(), pos.getZ() + 0.5F, igniter);
+            worldIn.spawnEntity(entitytntprimed);
+            worldIn.playSound((EntityPlayer) null, entitytntprimed.posX, entitytntprimed.posY, entitytntprimed.posZ, SoundEvents.ENTITY_TNT_PRIMED, SoundCategory.BLOCKS, 1.0F, 1.0F);
+        }
+    }
+
+    @Override
+    public void onBlockDestroyedByExplosion (World worldIn, BlockPos pos, Explosion explosionIn) {
+
+        if (!worldIn.isRemote) {
+
+            final EntityFakeTNT entitytntprimed = new EntityFakeTNT(worldIn, pos.getX() + 0.5F, pos.getY(), pos.getZ() + 0.5F, explosionIn.getExplosivePlacedBy());
+            entitytntprimed.setFuse((short) (worldIn.rand.nextInt(entitytntprimed.getFuse() / 4) + entitytntprimed.getFuse() / 8));
+            worldIn.spawnEntity(entitytntprimed);
         }
     }
 }
