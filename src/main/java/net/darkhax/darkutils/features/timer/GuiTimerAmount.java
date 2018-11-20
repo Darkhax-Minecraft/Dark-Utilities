a6dc389ee806a71032993de1c9968ddc2e41d694
Sewef <elagroum@gmail.com>
2018-11-20 13:46:01 +0100

    grmbl

--- src/main/java/net/darkhax/darkutils/features/timer/GuiTimerAmount.java
+++ src/main/java/net/darkhax/darkutils/features/timer/GuiTimerAmount.java
@@ -51,7 +51,7 @@ public class GuiTimerAmount extends GuiScreen {
         this.delayTextField.setText("" + this.timer.getDelayTime());
         this.doneBtn.enabled = this.delayTextField.getText().trim().length() > 0 && StringUtils.isNumeric(this.delayTextField.getText());
         
-    this.buttonList.add(this.cancelBtn = new GuiButton(2, this.width / 2 + 150, this.height / 2, 20, 40, !this.disabled ? "ON" : "OFF"));
+        this.buttonList.add(this.cancelBtn = new GuiButton(2, this.width / 2 + 150, this.height / 2, 20, 40, !this.disabled ? "ON" : "OFF"));
     }
 
     @Override
@@ -64,10 +64,10 @@ public class GuiTimerAmount extends GuiScreen {
     protected void actionPerformed (GuiButton button) throws IOException {
 
         if (button.enabled) {
-        if (button.id == 2) {
-            this.disabled ^= 1;
-        button.setText(!this.disabled ? "ON" : "OFF");
-        }
+            if (button.id == 2) {
+                this.disabled ^= 1;
+                button.setText(!this.disabled ? "ON" : "OFF");
+            }
             else if (button.id == 1) {
                 this.mc.displayGuiScreen((GuiScreen) null);
             }
