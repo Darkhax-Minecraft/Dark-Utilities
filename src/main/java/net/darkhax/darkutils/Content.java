b745e44e01c1ef31311cf29a2de56c0413b82849
Tyler Hancock <darklime@live.ca>
2019-07-14 11:27:10 -0600

    Added the various trap tiles. Added five new ones.

--- src/main/java/net/darkhax/darkutils/Content.java
+++ src/main/java/net/darkhax/darkutils/Content.java
@@ -4,12 +4,8 @@ import net.darkhax.bookshelf.registry.RegistryHelper;
 import net.darkhax.darkutils.features.flatblocks.BlockFlatTile;
 import net.darkhax.darkutils.features.flatblocks.BlockFlatTileRotating;
 import net.darkhax.darkutils.features.flatblocks.BlockFlatTileRotatingTicking;
+import net.darkhax.darkutils.features.flatblocks.TileEffects;
 import net.darkhax.darkutils.features.flatblocks.TileEntityTickingEffect;
-import net.darkhax.darkutils.features.flatblocks.collision.CollisionEffect;
-import net.darkhax.darkutils.features.flatblocks.collision.CollisionEffectImport;
-import net.darkhax.darkutils.features.flatblocks.collision.CollisionEffectPush;
-import net.darkhax.darkutils.features.flatblocks.tick.TickEffect;
-import net.darkhax.darkutils.features.flatblocks.tick.TickEffectExport;
 import net.darkhax.darkutils.features.slimecrucible.BlockSlimeCrucible;
 import net.darkhax.darkutils.features.slimecrucible.ContainerSlimeCrucible;
 import net.darkhax.darkutils.features.slimecrucible.RecipeSlimeCrafting;
@@ -60,6 +56,17 @@ public class Content {
     public final Block slimeCrucibleGreen;
     public final Block slimeCrucibleMagma;
     
+    public final Block runePoison;
+    public final Block runeWeakness;
+    public final Block runeSlowness;
+    public final Block runeWither;
+    public final Block runeFire;
+    public final Block runeFatigue;
+    public final Block runeGlowing;
+    public final Block runeHunger;
+    public final Block runeBlindness;
+    public final Block runeNausea;
+    
     /**
      * TILE ENTITIES
      */
@@ -79,24 +86,30 @@ public class Content {
         this.recipeSerializerSlimeFood = registry.registerRecipeSerializer(RecipeSlimeFood.SERIALIZER, "slime_food");
         this.recipeSerializerSlimeCrafting = registry.registerRecipeSerializer(RecipeSlimeCrafting.SERIALIZER, "slime_crafting");
         
-        final CollisionEffect effectVectorPlate = new CollisionEffectPush(0.06d);
-        final CollisionEffect effectVectorPlateFast = new CollisionEffectPush(0.3d);
-        final CollisionEffect effectVectorPlateExtreme = new CollisionEffectPush(1.5d);
-        final TickEffect exportEffect = new TickEffectExport();
-        
         this.blankPlate = registry.registerBlock(new BlockFlatTile(), "blank_plate");
         
-        this.vectorPlate = registry.registerBlock(new BlockFlatTileRotating(effectVectorPlate), "vector_plate");
-        this.exportPlate = registry.registerBlock(new BlockFlatTileRotatingTicking(effectVectorPlate, exportEffect, 10), "export_plate");
-        this.importPlate = registry.registerBlock(new BlockFlatTileRotating(new CollisionEffectImport(0.06d, 1)), "import_plate");
+        this.vectorPlate = registry.registerBlock(new BlockFlatTileRotating(TileEffects.PUSH_WEAK), "vector_plate");
+        this.exportPlate = registry.registerBlock(new BlockFlatTileRotatingTicking(TileEffects.PUSH_WEAK, TileEffects.EXPORT_INVENTORY, 10), "export_plate");
+        this.importPlate = registry.registerBlock(new BlockFlatTileRotating(TileEffects.IMPORT_WEAK), "import_plate");
         
-        this.vectorPlateFast = registry.registerBlock(new BlockFlatTileRotating(effectVectorPlateFast), "vector_plate_fast");
-        this.exportPlateFast = registry.registerBlock(new BlockFlatTileRotatingTicking(effectVectorPlateFast, exportEffect, 5), "export_plate_fast");
-        this.importPlateFast = registry.registerBlock(new BlockFlatTileRotating(new CollisionEffectImport(0.3d, 16)), "import_plate_fast");
+        this.vectorPlateFast = registry.registerBlock(new BlockFlatTileRotating(TileEffects.PUSH_NORMAL), "vector_plate_fast");
+        this.exportPlateFast = registry.registerBlock(new BlockFlatTileRotatingTicking(TileEffects.PUSH_NORMAL, TileEffects.EXPORT_INVENTORY, 5), "export_plate_fast");
+        this.importPlateFast = registry.registerBlock(new BlockFlatTileRotating(TileEffects.IMPORT_NORMAL), "import_plate_fast");
         
-        this.vectorPlateHyper = registry.registerBlock(new BlockFlatTileRotating(effectVectorPlateExtreme), "vector_plate_extreme");
-        this.exportPlateHyper = registry.registerBlock(new BlockFlatTileRotatingTicking(effectVectorPlateExtreme, exportEffect, 1), "export_plate_extreme");
-        this.importPlateHyper = registry.registerBlock(new BlockFlatTileRotating(new CollisionEffectImport(1.5d, 32)), "import_plate_extreme");
+        this.vectorPlateHyper = registry.registerBlock(new BlockFlatTileRotating(TileEffects.PUSH_STRONG), "vector_plate_extreme");
+        this.exportPlateHyper = registry.registerBlock(new BlockFlatTileRotatingTicking(TileEffects.PUSH_STRONG, TileEffects.EXPORT_INVENTORY, 1), "export_plate_extreme");
+        this.importPlateHyper = registry.registerBlock(new BlockFlatTileRotating(TileEffects.IMPORT_STRONG), "import_plate_extreme");
+        
+        this.runePoison = registry.registerBlock(new BlockFlatTile(TileEffects.RUNE_POISON), "rune_poison");
+        this.runeWeakness = registry.registerBlock(new BlockFlatTile(TileEffects.RUNE_WEAKNESS), "rune_weakness");
+        this.runeSlowness = registry.registerBlock(new BlockFlatTile(TileEffects.RUNE_SLOWNESS), "rune_slowness");
+        this.runeWither = registry.registerBlock(new BlockFlatTile(TileEffects.RUNE_WITHER), "rune_wither");
+        this.runeFire = registry.registerBlock(new BlockFlatTile(TileEffects.RUNE_FIRE), "rune_fire");
+        this.runeFatigue = registry.registerBlock(new BlockFlatTile(TileEffects.RUNE_FATIGUE), "rune_fatigue");
+        this.runeGlowing = registry.registerBlock(new BlockFlatTile(TileEffects.RUNE_GLOWING), "rune_glowing");
+        this.runeHunger = registry.registerBlock(new BlockFlatTile(TileEffects.RUNE_HUNGER), "rune_hunger");
+        this.runeBlindness = registry.registerBlock(new BlockFlatTile(TileEffects.RUNE_BLINDNESS), "rune_blindness");
+        this.runeNausea = registry.registerBlock(new BlockFlatTile(TileEffects.RUNE_NAUSEA), "rune_nausea");
         
         this.slimeCrucibleGreen = registry.registerBlock(new BlockSlimeCrucible(Properties.create(Material.CLAY, MaterialColor.GRASS).slipperiness(0.8F).hardnessAndResistance(0.6F).sound(SoundType.SLIME), SlimeCrucibleType.GREEN), "slime_crucible_green");
         this.slimeCrucibleMagma = registry.registerBlock(new BlockSlimeCrucible(Properties.create(Material.ROCK, MaterialColor.NETHERRACK).lightValue(3).hardnessAndResistance(0.5F), SlimeCrucibleType.MAGMA), "slime_crucible_magma");
---
+++ src/main/java/net/darkhax/darkutils/features/flatblocks/TileEffects.java
@@ -0,0 +1,118 @@
+package net.darkhax.darkutils.features.flatblocks;
+
+import net.darkhax.darkutils.features.flatblocks.collision.CollisionEffect;
+import net.darkhax.darkutils.features.flatblocks.collision.CollisionEffectImport;
+import net.darkhax.darkutils.features.flatblocks.collision.CollisionEffectPush;
+import net.darkhax.darkutils.features.flatblocks.tick.TickEffect;
+import net.darkhax.darkutils.features.flatblocks.tick.TickEffectExport;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class TileEffects {
+    
+    public static final CollisionEffect PUSH_WEAK = new CollisionEffectPush(0.06d);
+    public static final CollisionEffect PUSH_NORMAL = new CollisionEffectPush(0.3d);
+    public static final CollisionEffect PUSH_STRONG = new CollisionEffectPush(1.5d);
+    
+    public static final CollisionEffect IMPORT_WEAK = new CollisionEffectImport(0.06d, 1);
+    public static final CollisionEffect IMPORT_NORMAL = new CollisionEffectImport(0.3d, 16);
+    public static final CollisionEffect IMPORT_STRONG = new CollisionEffectImport(1.5d, 32);
+    
+    public static final CollisionEffect RUNE_POISON = TileEffects::effectRunePoison;
+    public static final CollisionEffect RUNE_WEAKNESS = TileEffects::effectRuneWeakness;
+    public static final CollisionEffect RUNE_SLOWNESS = TileEffects::effectRuneSlowness;
+    public static final CollisionEffect RUNE_WITHER = TileEffects::effectRuneWither;
+    public static final CollisionEffect RUNE_FIRE = TileEffects::effectRuneFire;
+    public static final CollisionEffect RUNE_FATIGUE = TileEffects::effectRuneFatigue;
+    public static final CollisionEffect RUNE_GLOWING = TileEffects::effectRuneGlowing;
+    public static final CollisionEffect RUNE_HUNGER = TileEffects::effectRuneHunger;
+    public static final CollisionEffect RUNE_BLINDNESS = TileEffects::effectRuneBlindess;
+    public static final CollisionEffect RUNE_NAUSEA = TileEffects::effectRuneNausea;
+    
+    public static final TickEffect EXPORT_INVENTORY = new TickEffectExport();
+    
+    private static void effectRunePoison (BlockState state, World world, BlockPos pos, Entity entity) {
+        
+        if (entity instanceof LivingEntity) {
+            
+            ((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.POISON, 100));
+        }
+    }
+    
+    private static void effectRuneWeakness (BlockState state, World world, BlockPos pos, Entity entity) {
+        
+        if (entity instanceof LivingEntity) {
+            
+            ((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.WEAKNESS, 100));
+        }
+    }
+    
+    private static void effectRuneSlowness (BlockState state, World world, BlockPos pos, Entity entity) {
+        
+        if (entity instanceof LivingEntity) {
+            
+            ((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.SLOWNESS, 100));
+        }
+    }
+    
+    private static void effectRuneWither (BlockState state, World world, BlockPos pos, Entity entity) {
+        
+        if (entity instanceof LivingEntity) {
+            
+            ((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.WITHER, 100));
+        }
+    }
+    
+    private static void effectRuneFire (BlockState state, World world, BlockPos pos, Entity entity) {
+        
+        if (entity instanceof LivingEntity) {
+            
+            ((LivingEntity) entity).setFire(4);
+        }
+    }
+    
+    private static void effectRuneFatigue (BlockState state, World world, BlockPos pos, Entity entity) {
+        
+        if (entity instanceof LivingEntity) {
+            
+            ((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.MINING_FATIGUE, 500));
+        }
+    }
+    
+    private static void effectRuneGlowing (BlockState state, World world, BlockPos pos, Entity entity) {
+        
+        if (entity instanceof LivingEntity) {
+            
+            ((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.GLOWING, 200));
+        }
+    }
+    
+    private static void effectRuneHunger (BlockState state, World world, BlockPos pos, Entity entity) {
+        
+        if (entity instanceof LivingEntity) {
+            
+            ((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.HUNGER, 100, 1));
+        }
+    }
+    
+    private static void effectRuneBlindess (BlockState state, World world, BlockPos pos, Entity entity) {
+        
+        if (entity instanceof LivingEntity) {
+            
+            ((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.BLINDNESS, 200));
+        }
+    }
+    
+    private static void effectRuneNausea (BlockState state, World world, BlockPos pos, Entity entity) {
+        
+        if (entity instanceof LivingEntity) {
+            
+            ((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.NAUSEA, 100));
+        }
+    }
+}
