488f1bed3ac3ba8bc6953daba0b89f412d0e1f77
Tyler Hancock <darklime@live.ca>
2019-07-24 15:24:19 -0600

    Ported to MC 1.14.4

--- src/main/java/net/darkhax/darkutils/features/slimecrucible/BlockSlimeCrucible.java
+++ src/main/java/net/darkhax/darkutils/features/slimecrucible/BlockSlimeCrucible.java
@@ -20,8 +20,8 @@ import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
-import net.minecraft.world.ServerWorld;
 import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
 
 public class BlockSlimeCrucible extends ContainerBlock implements ISlimeCrucibleBlock {
     
