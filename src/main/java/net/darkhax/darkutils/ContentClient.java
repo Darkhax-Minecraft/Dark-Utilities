e9eb2700cd78b8b86e0bbb0654b5ae3c2ecb257d
Tyler Hancock <darklime@live.ca>
2019-11-12 13:02:19 -0700

    Made vector plate and other similar blocks have full brightness models.

--- src/main/java/net/darkhax/darkutils/ContentClient.java
+++ src/main/java/net/darkhax/darkutils/ContentClient.java
@@ -1,5 +1,6 @@
 package net.darkhax.darkutils;
 
+import net.darkhax.bookshelf.client.model.FullbrightBakedModel;
 import net.darkhax.bookshelf.registry.RegistryHelper;
 import net.darkhax.bookshelf.registry.RegistryHelperClient;
 import net.darkhax.darkutils.features.slimecrucible.RenderSlimeCrucible;
@@ -7,6 +8,7 @@ import net.darkhax.darkutils.features.slimecrucible.ScreenSlimeCrucible;
 import net.darkhax.darkutils.features.slimecrucible.TileEntitySlimeCrucible;
 import net.darkhax.darkutils.features.witherslime.EntitySlimeWither;
 import net.darkhax.darkutils.features.witherslime.RenderSlimeWither;
+import net.minecraft.block.Block;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
@@ -32,6 +34,29 @@ public class ContentClient extends Content {
             
             // Entities
             clientRegistry.registerEntityRenderer(EntitySlimeWither.class, RenderSlimeWither::new);
+            
+            // Block Model Overrides
+            this.makeFullBrightFlatTile(clientRegistry, this.vectorPlate);
+            this.makeFullBrightFlatTile(clientRegistry, this.vectorPlateFast);
+            this.makeFullBrightFlatTile(clientRegistry, this.vectorPlateHyper);
+            this.makeFullBrightFlatTile(clientRegistry, this.importPlate);
+            this.makeFullBrightFlatTile(clientRegistry, this.importPlateFast);
+            this.makeFullBrightFlatTile(clientRegistry, this.importPlateHyper);
+            this.makeFullBrightFlatTile(clientRegistry, this.exportPlate);
+            this.makeFullBrightFlatTile(clientRegistry, this.exportPlateFast);
+            this.makeFullBrightFlatTile(clientRegistry, this.exportPlateHyper);
+            this.makeFullBrightFlatTile(clientRegistry, this.runeDamage);
+            this.makeFullBrightFlatTile(clientRegistry, this.runeDamagePlayer);
+            this.makeFullBrightFlatTile(clientRegistry, this.runePoison);
+            this.makeFullBrightFlatTile(clientRegistry, this.runeWeakness);
+            this.makeFullBrightFlatTile(clientRegistry, this.runeSlowness);
+            this.makeFullBrightFlatTile(clientRegistry, this.runeWither);
+            this.makeFullBrightFlatTile(clientRegistry, this.runeFire);
+            this.makeFullBrightFlatTile(clientRegistry, this.runeFatigue);
+            this.makeFullBrightFlatTile(clientRegistry, this.runeGlowing);
+            this.makeFullBrightFlatTile(clientRegistry, this.runeHunger);
+            this.makeFullBrightFlatTile(clientRegistry, this.runeBlindness);
+            this.makeFullBrightFlatTile(clientRegistry, this.runeNausea);
         }
         
         MinecraftForge.EVENT_BUS.addListener(EventPriority.HIGHEST, this::addTooltips);
@@ -45,5 +70,14 @@ public class ContentClient extends Content {
             
             event.getToolTip().add(new TranslationTextComponent("tooltip.darkutils." + id.getPath() + ".short").applyTextStyle(TextFormatting.GRAY));
         }
+    }
+    
+    private void makeFullBrightFlatTile (RegistryHelperClient registry, Block tile) {
+        
+        final ResourceLocation blockId = tile.getRegistryName();
+        registry.registerModelFactory(blockId, (original, ctx) -> {
+            
+            return new FullbrightBakedModel(original, 0.007f, new ResourceLocation(blockId.getNamespace(), "block/" + blockId.getPath()));
+        });
     }
 }
