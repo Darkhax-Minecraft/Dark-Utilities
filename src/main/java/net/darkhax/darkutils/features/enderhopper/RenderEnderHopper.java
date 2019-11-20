4f2f6e7ad3f6e28b19b87e6ceea3130fad434917
Tyler Hancock <darklime@live.ca>
2019-11-20 12:10:00 -0700

    Improved ender hopper border render. Thanks to Jared.

--- src/main/java/net/darkhax/darkutils/features/enderhopper/RenderEnderHopper.java
+++ src/main/java/net/darkhax/darkutils/features/enderhopper/RenderEnderHopper.java
@@ -1,16 +1,16 @@
 package net.darkhax.darkutils.features.enderhopper;
 
 import com.mojang.blaze3d.platform.GlStateManager;
-
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
+import net.minecraft.block.*;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.item.ItemStack;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraft.util.*;
+import net.minecraftforge.api.distmarker.*;
 import net.minecraftforge.client.model.animation.TileEntityRendererFast;
+
+import java.util.*;
 
 @OnlyIn(Dist.CLIENT)
 public class RenderEnderHopper extends TileEntityRendererFast<TileEntityEnderHopper> {
@@ -18,70 +18,80 @@ public class RenderEnderHopper extends TileEntityRendererFast<TileEntityEnderHop
     @Override
     public void renderTileEntityFast(TileEntityEnderHopper tile, double x, double y, double z, float partialTicks, int destroyStage, BufferBuilder buffer) {
         final BlockState state = tile.getWorld().getBlockState(tile.getPos());
-    
-        if (state.get(BlockEnderHopper.SHOW_BORDER)) {
         
+        if(state.get(BlockEnderHopper.SHOW_BORDER)) {
+            
             GlStateManager.pushMatrix();
-            // 0.00001 to avoid z-fighting
-            renderCubeOverlay(9f - 0.001f, x, y, z, 0xffffff , buffer);
+            renderCubeOverlay(9f + 0.001f, x, y, z, 0xffffff, buffer);
             GlStateManager.popMatrix();
         }
     }
     
-    private static void renderCubeOverlay (float box, double x, double y, double z, int color, BufferBuilder buffer) {
+    private static TextureAtlasSprite icon;
+    
+    private static void renderCubeOverlay(float box, double x, double y, double z, int color, BufferBuilder buffer) {
         
-        @SuppressWarnings("deprecation") 
-        TextureAtlasSprite icon = Minecraft.getInstance().getItemRenderer().getItemModelMesher().getItemModel(new ItemStack(Blocks.PURPLE_STAINED_GLASS)).getParticleTexture();
+        if (icon == null) {
+            
+            icon = Minecraft.getInstance().getTextureMap().getSprite(new ResourceLocation("minecraft", "block/nether_portal"));
+        }
+        
         buffer.setTranslation(x + 0.5, y + 0.5, z + 0.5);
-        drawOverlay(box, box, box, icon, color, 255, buffer);
+        drawOverlay(box, box, box, icon, color, 255, buffer);        
+    }
+    
+    private static Map<Direction, Renderable> directionMap = Util.make(new HashMap<>(), map -> {
+        map.put(Direction.UP, (buffer, width, height, length, icon, red, green, blue, alpha, light1, light2) -> {
+            buffer.pos(-(width / 2), (height / 2), -(length / 2)).color(red, green, blue, alpha).tex(icon.getMinU(), icon.getMinV()).lightmap(light1, light2).endVertex();
+            buffer.pos((width / 2), (height / 2), -(length / 2)).color(red, green, blue, alpha).tex(icon.getMaxU(), icon.getMinV()).lightmap(light1, light2).endVertex();
+            buffer.pos((width / 2), (height / 2), (length / 2)).color(red, green, blue, alpha).tex(icon.getMaxU(), icon.getMaxV()).lightmap(light1, light2).endVertex();
+            buffer.pos(-(width / 2), (height / 2), (length / 2)).color(red, green, blue, alpha).tex(icon.getMinU(), icon.getMaxV()).lightmap(light1, light2).endVertex();
+        });
+        map.put(Direction.DOWN, (buffer, width, height, length, icon, red, green, blue, alpha, light1, light2) -> {
+            buffer.pos(-(width / 2), -(height / 2), (length / 2)).color(red, green, blue, alpha).tex(icon.getMinU(), icon.getMaxV()).lightmap(light1, light2).endVertex();
+            buffer.pos((width / 2), -(height / 2), (length / 2)).color(red, green, blue, alpha).tex(icon.getMaxU(), icon.getMaxV()).lightmap(light1, light2).endVertex();
+            buffer.pos((width / 2), -(height / 2), -(length / 2)).color(red, green, blue, alpha).tex(icon.getMaxU(), icon.getMinV()).lightmap(light1, light2).endVertex();
+            buffer.pos(-(width / 2), -(height / 2), -(length / 2)).color(red, green, blue, alpha).tex(icon.getMinU(), icon.getMinV()).lightmap(light1, light2).endVertex();
+        });
+        map.put(Direction.NORTH, (buffer, width, height, length, icon, red, green, blue, alpha, light1, light2) -> {
+            buffer.pos((width / 2), -(height / 2), (length / 2)).color(red, green, blue, alpha).tex(icon.getMaxU(), icon.getMinV()).lightmap(light1, light2).endVertex();
+            buffer.pos(-(width / 2), -(height / 2), (length / 2)).color(red, green, blue, alpha).tex(icon.getMinU(), icon.getMinV()).lightmap(light1, light2).endVertex();
+            buffer.pos(-(width / 2), (height / 2), (length / 2)).color(red, green, blue, alpha).tex(icon.getMinU(), icon.getMaxV()).lightmap(light1, light2).endVertex();
+            buffer.pos((width / 2), (height / 2), (length / 2)).color(red, green, blue, alpha).tex(icon.getMaxU(), icon.getMaxV()).lightmap(light1, light2).endVertex();
+        });
+        map.put(Direction.SOUTH, (buffer, width, height, length, icon, red, green, blue, alpha, light1, light2) -> {
+            buffer.pos(-(width / 2), -(height / 2), -(length / 2)).color(red, green, blue, alpha).tex(icon.getMinU(), icon.getMinV()).lightmap(light1, light2).endVertex();
+            buffer.pos((width / 2), -(height / 2), -(length / 2)).color(red, green, blue, alpha).tex(icon.getMaxU(), icon.getMinV()).lightmap(light1, light2).endVertex();
+            buffer.pos((width / 2), (height / 2), -(length / 2)).color(red, green, blue, alpha).tex(icon.getMaxU(), icon.getMaxV()).lightmap(light1, light2).endVertex();
+            buffer.pos(-(width / 2), (height / 2), -(length / 2)).color(red, green, blue, alpha).tex(icon.getMinU(), icon.getMaxV()).lightmap(light1, light2).endVertex();
+        });
+        map.put(Direction.EAST, (buffer, width, height, length, icon, red, green, blue, alpha, light1, light2) -> {
+            buffer.pos(-(width / 2), (height / 2), -(length / 2)).color(red, green, blue, alpha).tex(icon.getMinU(), icon.getMaxV()).lightmap(light1, light2).endVertex();
+            buffer.pos(-(width / 2), (height / 2), (length / 2)).color(red, green, blue, alpha).tex(icon.getMaxU(), icon.getMaxV()).lightmap(light1, light2).endVertex();
+            buffer.pos(-(width / 2), -(height / 2), (length / 2)).color(red, green, blue, alpha).tex(icon.getMaxU(), icon.getMinV()).lightmap(light1, light2).endVertex();
+            buffer.pos(-(width / 2), -(height / 2), -(length / 2)).color(red, green, blue, alpha).tex(icon.getMinU(), icon.getMinV()).lightmap(light1, light2).endVertex();
+        });
+        map.put(Direction.WEST, (buffer, width, height, length, icon, red, green, blue, alpha, light1, light2) -> {
+            buffer.pos((width / 2), -(height / 2), -(length / 2)).color(red, green, blue, alpha).tex(icon.getMinU(), icon.getMinV()).lightmap(light1, light2).endVertex();
+            buffer.pos((width / 2), -(height / 2), (length / 2)).color(red, green, blue, alpha).tex(icon.getMaxU(), icon.getMinV()).lightmap(light1, light2).endVertex();
+            buffer.pos((width / 2), (height / 2), (length / 2)).color(red, green, blue, alpha).tex(icon.getMaxU(), icon.getMaxV()).lightmap(light1, light2).endVertex();
+            buffer.pos((width / 2), (height / 2), -(length / 2)).color(red, green, blue, alpha).tex(icon.getMinU(), icon.getMaxV()).lightmap(light1, light2).endVertex();
+        });
+    });
+    
+    public static void drawOverlay(float width, float height, float length, TextureAtlasSprite icon, int color, int alpha, BufferBuilder buffer) {
+        
+        Direction[] directions = Direction.getFacingDirections(Minecraft.getInstance().renderViewEntity);
+        
+        for(Direction direction : directions) {
+            Renderable renderable = directionMap.get(direction.getAxis().isHorizontal() ? direction : direction.getOpposite());
+            renderable.render(buffer, width, height, length, icon, color >> 16 & 255, color >> 8 & 255, color & 255, alpha, 200, 200);
+        }
         
     }
     
-    public static void drawOverlay (float width, float height, float length, TextureAtlasSprite icon, int color, int alpha, BufferBuilder buffer) {
-    
+    private interface Renderable {
         
-        int r = color >> 16 & 255;
-        int g = color >> 8 & 255;
-        int b = color & 255;
-    
-        int light1 = 200;
-        int light2 = 200;
-    
-        // TOP
-        buffer.pos(-(width / 2), (height / 2), -(length / 2)).color(r, g, b, alpha).tex(icon.getMinU(), icon.getMinV()).lightmap(light1, light2).endVertex();
-        buffer.pos((width / 2), (height / 2), -(length / 2)).color(r, g, b, alpha).tex(icon.getMaxU(), icon.getMinV()).lightmap(light1, light2).endVertex();
-        buffer.pos((width / 2), (height / 2), (length / 2)).color(r, g, b, alpha).tex(icon.getMaxU(), icon.getMaxV()).lightmap(light1, light2).endVertex();
-        buffer.pos(-(width / 2), (height / 2), (length / 2)).color(r, g, b, alpha).tex(icon.getMinU(), icon.getMaxV()).lightmap(light1, light2).endVertex();
-        
-        // BOTTOM
-        buffer.pos(-(width / 2), -(height / 2), (length / 2)).color(r, g, b, alpha).tex(icon.getMinU(), icon.getMaxV()).lightmap(light1, light2).endVertex();
-        buffer.pos((width / 2), -(height / 2), (length / 2)).color(r, g, b, alpha).tex(icon.getMaxU(), icon.getMaxV()).lightmap(light1, light2).endVertex();
-        buffer.pos((width / 2), -(height / 2), -(length / 2)).color(r, g, b, alpha).tex(icon.getMaxU(), icon.getMinV()).lightmap(light1, light2).endVertex();
-        buffer.pos(-(width / 2), -(height / 2), -(length / 2)).color(r, g, b, alpha).tex(icon.getMinU(), icon.getMinV()).lightmap(light1, light2).endVertex();
-        
-        // NORTH
-        buffer.pos((width / 2), -(height / 2), (length / 2)).color(r, g, b, alpha).tex(icon.getMaxU(), icon.getMinV()).lightmap(light1, light2).endVertex();
-        buffer.pos(-(width / 2), -(height / 2), (length / 2)).color(r, g, b, alpha).tex(icon.getMinU(), icon.getMinV()).lightmap(light1, light2).endVertex();
-        buffer.pos(-(width / 2), (height / 2), (length / 2)).color(r, g, b, alpha).tex(icon.getMinU(), icon.getMaxV()).lightmap(light1, light2).endVertex();
-        buffer.pos((width / 2), (height / 2), (length / 2)).color(r, g, b, alpha).tex(icon.getMaxU(), icon.getMaxV()).lightmap(light1, light2).endVertex();
-        
-        // SOUTH
-        buffer.pos(-(width / 2), -(height / 2), -(length / 2)).color(r, g, b, alpha).tex(icon.getMinU(), icon.getMinV()).lightmap(light1, light2).endVertex();
-        buffer.pos((width / 2), -(height / 2), -(length / 2)).color(r, g, b, alpha).tex(icon.getMaxU(), icon.getMinV()).lightmap(light1, light2).endVertex();
-        buffer.pos((width / 2), (height / 2), -(length / 2)).color(r, g, b, alpha).tex(icon.getMaxU(), icon.getMaxV()).lightmap(light1, light2).endVertex();
-        buffer.pos(-(width / 2), (height / 2), -(length / 2)).color(r, g, b, alpha).tex(icon.getMinU(), icon.getMaxV()).lightmap(light1, light2).endVertex();
-    
-        // EAST
-        buffer.pos(-(width / 2), (height / 2), -(length / 2)).color(r, g, b, alpha).tex(icon.getMinU(), icon.getMaxV()).lightmap(light1, light2).endVertex();
-        buffer.pos(-(width / 2), (height / 2), (length / 2)).color(r, g, b, alpha).tex(icon.getMaxU(), icon.getMaxV()).lightmap(light1, light2).endVertex();
-        buffer.pos(-(width / 2), -(height / 2), (length / 2)).color(r, g, b, alpha).tex(icon.getMaxU(), icon.getMinV()).lightmap(light1, light2).endVertex();
-        buffer.pos(-(width / 2), -(height / 2), -(length / 2)).color(r, g, b, alpha).tex(icon.getMinU(), icon.getMinV()).lightmap(light1, light2).endVertex();
-    
-        // WEST
-        buffer.pos((width / 2), -(height / 2), -(length / 2)).color(r, g, b, alpha).tex(icon.getMinU(), icon.getMinV()).lightmap(light1, light2).endVertex();
-        buffer.pos((width / 2), -(height / 2), (length / 2)).color(r, g, b, alpha).tex(icon.getMaxU(), icon.getMinV()).lightmap(light1, light2).endVertex();
-        buffer.pos((width / 2), (height / 2), (length / 2)).color(r, g, b, alpha).tex(icon.getMaxU(), icon.getMaxV()).lightmap(light1, light2).endVertex();
-        buffer.pos((width / 2), (height / 2), -(length / 2)).color(r, g, b, alpha).tex(icon.getMinU(), icon.getMaxV()).lightmap(light1, light2).endVertex();
-    
+        void render(BufferBuilder buffer, float width, float height, float length, TextureAtlasSprite icon, int red, int blue, int green, int alpha, int light1, int light2);
     }
 }
